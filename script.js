aa3beae68bf1c8450248ba4e994c4f14a137cda8
Lee <leenock2012@yahoo.co.uk>
2021-07-17 11:56:58 +0100

    Java functionality

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
- 
-
-var artist = "";
+var artist = $(".input");
 var APIKey = '272852FTBBwuqJtoUuXgJwvesHROoz66uEYbSRJQBT67Y3fbtz64KCA8'
-var savedArtist = $(".search-btn")
-
-$("#search-btn").click(function (event) {
-    event.preventDefault();
-    artist = $("#search-btn").val();
-    console.log(event);
-});
+var searchBtn = $("#search-btn")
+var results = $(".results-card")
 
 
+// $("#search-btn").click(function (event) {
+//     event.preventDefault();
+//     artist = $("#search-btn").val();
+//     console.log(event);
+// });
 
+function getResponse(event) {
+    event.preventDefault()
 
+    fetch(`https://api.happi.dev/v1/music?q=${artist.val()}&limit=10&apikey=${APIKey}&lyrics=0`)
+.then(response => response.json())
+.then(function (data) {
+    console.log(data)
+    results.append(`<li>Results: ${data.result}`)
+})
+}
 
-function getApi(queryURL) {
-    fetch(queryURL)
-    
-    .then(function (response) {
-        console.log(response);
-        if (response.status === 200) {
-            responseText.textContent = response.status;
-          }
-          return response.json();
-      });
-    }
+searchBtn.on("click", getResponse)
 
-var queryURL = `https://api.happi.dev/v1/music?q=eminem&limit=10&apikey=${APIKey}&lyrics=0`
-getApi(queryURL);
+// function getApi(queryURL) {
+//     var queryURL = `https://api.happi.dev/v1/music?q=${artist}&limit=10&apikey=${APIKey}&lyrics=0`
+//     fetch(queryURL)
+//     .then(function (response) {
+//         console.log(response);
+//         if (response.status === 200) {
+//             response.textContent = response.status;
+//           }
+//           return response.json();
+//       });
+//       $(".results-card").append(`<li>${response.result}</li>`)
+// }
+
+//Display required response in HTML

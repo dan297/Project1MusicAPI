17711ba1afcf1a4289eb4bcd7945147b2a0b57cb
dan297 <71897967+dan297@users.noreply.github.com>
2021-07-19 23:04:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var artist = $(".input");
 var APIKey = '272852FTBBwuqJtoUuXgJwvesHROoz66uEYbSRJQBT67Y3fbtz64KCA8'
 var searchBtn = $("#search-btn")
-var results = $(".results-card")
-
+var results = $("#button0")
+var url = `https://api.happi.dev/v1/music?q=${artist.val()}&limit=10&apikey=${APIKey}&lyrics=0`
+var clearBtn = $("#clear-btn")
 
 // $("#search-btn").click(function (event) {
 //     event.preventDefault();
@@ -10,18 +11,45 @@ var results = $(".results-card")
 //     console.log(event);
 // });
 
-function getResponse(event) {
+function handleSearchClick(event) {
+    var searchTerm =  artist.val()
     event.preventDefault()
-
-    fetch(`https://api.happi.dev/v1/music?q=${artist.val()}&limit=10&apikey=${APIKey}&lyrics=0`)
-.then(response => response.json())
-.then(function (data) {
-    console.log(data)
-    results.append(`<li>Results: ${data.result}`)
-})
+    requestAPI(searchTerm);
+    addSearchHistory(searchTerm);
 }
 
-searchBtn.on("click", getResponse)
+function requestAPI(searchValue) {
+    fetch(`https://api.happi.dev/v1/music?q=${searchValue}&limit=10&apikey=${APIKey}&lyrics=0`)
+        .then(response => response.json())
+        .then(function (data) {
+            var artistTracks = data.result;
+            console.log(data);
+            // results.append(`<li>Results: ${artistTracks}`)
+            for (var i = 0; i < artistTracks.length; i++) {
+                var trackObject = artistTracks[i];
+                results.append(`<li> ${trackObject.track} ${trackObject.artist}`);
+            }
+        });
+}
+
+function addSearchHistory(artistName) {
+    var artistContainer = $("#box2")
+    artistContainer.append(`<li>- ${artistName}`)
+}
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+function clearHistory() {
+    results.empty()
+}
+
+clearBtn.on("click", clearHistory)
+searchBtn.on("click", handleSearchClick)
+
+
+    
 
 // function getApi(queryURL) {
 //     var queryURL = `https://api.happi.dev/v1/music?q=${artist}&limit=10&apikey=${APIKey}&lyrics=0`

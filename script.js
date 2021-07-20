bdfede5bf82a0b9aa27c74a557dfd5a936c2a623
Lee <leenock2012@yahoo.co.uk>
2021-07-20 21:08:45 +0100

    Cleaned the code after merge conflicts

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var artist = $(".input");
 var APIKey = '272852FTBBwuqJtoUuXgJwvesHROoz66uEYbSRJQBT67Y3fbtz64KCA8'
 var searchBtn = $("#search-btn")
-var url = `https://api.happi.dev/v1/music?q=${artist.val()}&limit=10&apikey=${APIKey}&lyrics=0`
 var clearBtn = $("#clear-btn")
 var searchSongBtn = $("#searchSong-btn")
 var songName = $(".songInput");
@@ -10,7 +9,6 @@ var urlTwo = `https://shazam.p.rapidapi.com/search?term=${songName.val()}&limit=
 var songresults = $("#button1")
 var results = $("#box1")
 var url = `https://api.happi.dev/v1/music?q=${artist.val()}&limit=10&apikey=${APIKey}&lyrics=0`
-var clearBtn = $("#clear-btn")
 
 function handleSearchClick(event) {
     var searchTerm =  artist.val()
@@ -21,26 +19,6 @@ function handleSearchClick(event) {
 
 function requestAPI(searchValue) {
     fetch(`https://api.happi.dev/v1/music?q=${searchValue}&limit=10&apikey=${APIKey}&lyrics=0`)
-        .then(response => response.json())
-        .then(function (data) {
-            var artistTracks = data.result;
-            console.log(data);
-            for (var i = 0; i < artistTracks.length; i++) {
-                var trackObject = artistTracks[i];
-                results.append(`<li> ${trackObject.track} ${trackObject.artist}`);
-            }
-        });
-}
-
-
-function handleSearchClick(event) {
-    var searchTerm =  artist.val()
-    event.preventDefault()
-    requestAPI(searchTerm);
-    addSearchHistory(searchTerm);
-}
-function requestAPI(searchTerm) {
-    fetch(`https://api.happi.dev/v1/music?q=${searchTerm}&limit=10&apikey=${APIKey}&lyrics=0`)
         .then(response => response.json())
         .then(function (data) {
             var artistTracks = data.result;

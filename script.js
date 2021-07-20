6c5e418473cbed4ef8af509060824a921f6bee37
Lee <leenock2012@yahoo.co.uk>
2021-07-20 18:29:32 +0100

    Cleaning the code a bit

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ var results = $("#button0")
 var url = `https://api.happi.dev/v1/music?q=${artist.val()}&limit=10&apikey=${APIKey}&lyrics=0`
 var clearBtn = $("#clear-btn")
 
-// $("#search-btn").click(function (event) {
-//     event.preventDefault();
-//     artist = $("#search-btn").val();
-//     console.log(event);
-// });
-
 function handleSearchClick(event) {
     var searchTerm =  artist.val()
     event.preventDefault()
@@ -24,7 +18,6 @@ function requestAPI(searchValue) {
         .then(function (data) {
             var artistTracks = data.result;
             console.log(data);
-            // results.append(`<li>Results: ${artistTracks}`)
             for (var i = 0; i < artistTracks.length; i++) {
                 var trackObject = artistTracks[i];
                 results.append(`<li> ${trackObject.track} ${trackObject.artist}`);
@@ -47,21 +40,3 @@ function clearHistory() {
 
 clearBtn.on("click", clearHistory)
 searchBtn.on("click", handleSearchClick)
-
-
-    
-
-// function getApi(queryURL) {
-//     var queryURL = `https://api.happi.dev/v1/music?q=${artist}&limit=10&apikey=${APIKey}&lyrics=0`
-//     fetch(queryURL)
-//     .then(function (response) {
-//         console.log(response);
-//         if (response.status === 200) {
-//             response.textContent = response.status;
-//           }
-//           return response.json();
-//       });
-//       $(".results-card").append(`<li>${response.result}</li>`)
-// }
-
-//Display required response in HTML
